3671fe079ffedcc17c1722f679b1f7097ce54615
Igarin <145533797+igarin14pm@users.noreply.github.com>
2026-03-15 12:30:00 +0900

    timers-promises.ts に `setTimeout()` を追加

---
+++ scripts/dist/timers-promises.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2026 Igarin
+// This software is released under the MIT License.
+// https://opensource.org
+export function setTimeout(time_ms) {
+    return new Promise((resolve) => window.setTimeout(resolve, time_ms));
+}
